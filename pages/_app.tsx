e50e210393cf3133efeb3e21d9d97333595a2b8f
unishigure <s.reiuniuni@gmail.com>
2023-10-04 10:55:03 +0900

    Update _app.tsx
    Font Optimization
    https://nextjs.org/docs/pages/building-your-application/optimizing/fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import "../styles.css";
+import { Noto_Sans_JP } from "next/font/google";
+
+const notoSansJP = Noto_Sans_JP({
+  subsets: ["latin", "latin-ext"],
+});
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={notoSansJP.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
