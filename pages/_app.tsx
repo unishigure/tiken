d83608aaf013a392814d6eeb46ff074125760877
unishigure <s.reiuniuni@gmail.com>
2023-10-04 13:12:06 +0900

    Add Web Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import { Noto_Sans_JP } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const notoSansJP = Noto_Sans_JP({
   subsets: ["latin", "latin-ext"],
 });
 
-// This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return (
     <main className={notoSansJP.className}>
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
